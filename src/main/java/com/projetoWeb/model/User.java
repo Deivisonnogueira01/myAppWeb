d34b20946936b0abc5a0be39e42617def3f119f1
Deivison Nogueira <deivison.nogueira@live.com>
2022-02-14 23:57:48 -0300

    test

---
+++ src/main/java/com/projetoWeb/model/User.java
@@ -0,0 +1,20 @@
+package com.projetoWeb.model;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    UUID id;
+    String nome;
+    String email;
+
+
+
+    
+}
---
+++ src/test/java/com/deivinho/deivinho/DeivinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deivinho.deivinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeivinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
