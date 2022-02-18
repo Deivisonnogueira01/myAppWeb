6dcdabcdee512445057994eeabffa313aa0a0826
Deivison Nogueira <deivison.nogueira@live.com>
2022-02-18 16:49:50 -0400

    teste1

--- project_web/src/main/java/com/web/frame/project_web/controllers/UserController.java
+++ project_web/src/main/java/com/web/frame/project_web/controllers/UserController.java
@@ -1,16 +1,22 @@
 package com.web.frame.project_web.controllers;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping(value = "/users")
 public class UserController {
     
 
-  //  @GetMapping("/teste")
-    //}
+    @GetMapping(value= "/userteste")
+    public ResponseEntity<Response> addEntity(){
+       return new ResponseEntity<>(HttpStatus.OK);
+    }
+               
     
     
 }
---
+++ project_web/src/main/java/com/web/frame/project_web/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.web.frame.project_web.repositories;
+
+/*import com.web.frame.project_web.model.User;
+
+public interface UserRepository  extends JpaRepository<User, Long>{
+    
+    public User findBy(String name);
+}
+*/
